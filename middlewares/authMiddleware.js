676cdb9415d5b10c371d5a4824c2419234377a06
Lepha <lepharamchiary@gmail.com>
2024-05-25 23:19:40 +0530

    simple changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,14 +1,19 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/user')
 
-const SECRET_KEY = 'YOUR_SECRET_KEY';
+const SECRET_KEY = process.env.SECRET_KEY;
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', '');
     if (!token) return res.status(401).json({ error: 'Access denied, no token provided' });
 
     try {
         const decoded = jwt.verify(token, SECRET_KEY);
-        req.user = decoded;
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid token' });
+        }
+        req.user = user;
         next();
     } catch (err) {
         res.status(400).json({ error: 'Invalid token' });
