0adddad019ba8da5cc8e900d48397bb5069104ea
Lepha <lepharamchiary@gmail.com>
2024-05-25 23:21:17 +0530

    simple changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,52 @@
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/user');
+
+// const SECRET_KEY = 'YOUR_SECRET_KEY';
+
+// exports.register = async (req, res) => {
+//     const { username, email, password, role } = req.body;
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     try {
+//         const user = new User({ username, email, password: hashedPassword , role: role || 'customer'});
+//         await user.save();
+//         res.status(201).json({ message: 'User registered successfully' });
+//     } catch (err) {
+//         res.status(500).json({ error: 'Error registering user' });
+//     }
+// };
+
+// exports.login = async (req, res) => {
+//     const { email, password } = req.body;
+
+//     try {
+//         const user = await User.findOne({ email });
+//         if (!user || !await bcrypt.compare(password, user.password)) {
+//             return res.status(401).json({ error: 'Invalid email or password' });
+//         }
+
+//         const token = jwt.sign({ userId: user._id, role: user.role }, SECRET_KEY, { expiresIn: '1h' });
+//         res.json({ token });
+//     } catch (err) {
+//         res.status(500).json({ error: 'Error logging in' });
+//     }
+// };
+
+
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const SECRET_KEY = 'YOUR_SECRET_KEY';
+const SECRET_KEY = process.env.SECRET_KEY;
 
 exports.register = async (req, res) => {
     const { username, email, password, role } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
-        const user = new User({ username, email, password: hashedPassword , role: role || 'customer'});
+        const user = new User({ username, email, password: hashedPassword, role: role || 'customer' });
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (err) {
@@ -32,3 +69,4 @@ exports.login = async (req, res) => {
         res.status(500).json({ error: 'Error logging in' });
     }
 };
+
